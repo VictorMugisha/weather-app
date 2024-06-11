cbaf87f60f3bb2e29052e7e49470a482c818a292
Victor Mugisha Shyaka <victormugishavm6@gmail.com>
2024-06-11 17:32:57 +0200

    Finished the first working draft

--- script.js
+++ script.js
@@ -4,17 +4,23 @@ const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q="
 
 const searchBtn = document.querySelector("#search-btn")
 const weatherIcon = document.querySelector(".weather-icon")
+const errorSection = document.querySelector(".error")
 
 searchBtn.addEventListener('click', () => {
     const inputCity = document.querySelector("#input").value
-    checkWeather(inputCity)   
+    checkWeather(inputCity)
 })
 
 async function checkWeather(cityName) {
     const response = await fetch(apiUrl + cityName + `&appid=${apiKey}`)
     let data = await response.json()
 
-    // console.log(data)
+    if (!response.ok || response.status !== 200) {
+        errorSection.style.display = "block"
+        document.querySelector(".weather").style.display = "none"
+        return;
+    }
+    console.log(data)
     if (data.weather[0].main === "Clouds") {
         weatherIcon.src = "images/clouds.png"
     } else if (data.weather[0].main === "Clear") {
@@ -33,6 +39,8 @@ async function checkWeather(cityName) {
     document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C"
     document.querySelector(".humidity").innerHTML = data.main.humidity + "%"
     document.querySelector(".wind").innerHTML = data.wind.speed + " km/h"
+
+    document.querySelector(".weather").style.display = "block"
 
 }
 
